862467e8e9add097e3044a7d7d6ad91d98616489
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-08-24 17:02:32 -0400

    Random failure.

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world!\n"
+  if Random.rand(5) == 4
+    body "Random failure, crashing."
+    logger.info "Randomly crashing the server in order to demonstrate adversity."
+    Thread.new { sleep 1; Process.kill 'INT', Process.pid }
+    halt 200
+  else
+    "Hello, world!\n"
+  end
 end
