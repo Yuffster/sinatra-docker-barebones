5131d09654cd4f0eb95822ca1c5952e2585d7fa4
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-08-24 17:08:33 -0400

    Live server coding, let's see if this fixes the thing.

--- app.rb
+++ app.rb
@@ -2,10 +2,9 @@ require 'sinatra'
 
 get '/' do
   if Random.rand(5) == 4
-    body "Random failure, crashing."
     logger.info "Randomly crashing the server in order to demonstrate adversity."
     Thread.new { sleep 1; Process.kill 'INT', Process.pid }
-    halt 200
+    body "Random failure, crashing."
   else
     "Hello, world!\n"
   end
